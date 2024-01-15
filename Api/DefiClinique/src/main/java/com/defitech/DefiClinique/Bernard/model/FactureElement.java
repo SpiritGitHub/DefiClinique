615dfa98cfc5a2dbce47a3d948dee519f3973e69
bernard <bernardabofla@gmail.com>
2024-01-15 20:38:33 +0000

    travail finie

---
+++ Api/DefiClinique/src/main/java/com/defitech/DefiClinique/Bernard/model/FactureElement.java
@@ -0,0 +1,39 @@
+package com.defitech.DefiClinique.Bernard.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Entity
+@Table(name = "facture_element")
+public class FactureElement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "facture_id", nullable = false)
+    private Facture facture;
+
+    @ManyToOne
+    @JoinColumn(name = "service_id", nullable = false)
+    private ClinicService clinicService;
+
+    @Column(name = "nom", nullable = false)
+    private String nom;
+
+    @Column(name = "prix", nullable = false)
+    private Double prix;
+
+    public FactureElement() {
+
+    }
+
+
+}
---
+++ Api/DefiClinique/src/main/java/com/defitech/DefiClinique/configurations/DatabaseConfig.java
@@ -0,0 +1,21 @@
+package com.defitech.DefiClinique.configurations;
+
+import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/deficlinique");
+        dataSource.setUsername("root");
+        dataSource.setPassword("");
+
+        return dataSource;
+    }
+}
